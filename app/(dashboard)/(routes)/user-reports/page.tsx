006dba18bf09d43d1a6a360440ff4b559c93a72d
Bùi Hữu Nghĩa <130369258+buihuunghia2004@users.noreply.github.com>
2024-11-11 13:54:18 +0700

    feat: user report

--- app/(dashboard)/(routes)/user-reports/page.tsx
+++ app/(dashboard)/(routes)/user-reports/page.tsx
@@ -1,19 +1,32 @@
 "use client";
+import axios from "axios";
 import {
     EllipsisVertical,
     SquareChevronLeft,
     SquareChevronRight,
 } from "lucide-react";
 import React, { useEffect, useState } from "react";
+import  AxiosInstance from "../../../config/axiosInstance";
+import { Select } from "@radix-ui/react-select";
+import { SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 const UserReportPage = () => {
-    const [data, setData] = useState(fakeDataUserReport);
+    //useState Report[]
+    const [data, setData]  = useState<Report[]>([]);
     const [showDialog, setShowDialog] = useState(false);
 
-    const pageTotal = Math.ceil(data.length / 10);
+    const pageTotal = Math.ceil((data?.length || 0) / 10);
     const [pageSelected, setPageSelected] = useState(1);
     const [indexS, setIndexS] = useState(0);
-    const [indexE, setIndexE] = useState(data.length < 9 ? data.length : 9);
+    const [indexE, setIndexE] = useState(data?.length < 9 ? data?.length : 9);
+    const [judge, setJudge] = useState("0");
+
+    useEffect(() => {
+        AxiosInstance().get("/report/reports?report_type=user")
+        .then((res) => {
+                setData(res.data.list)
+            });
+    }, []);
 
     const pagination = () => {
         const handlePrevious = () => {
@@ -91,19 +104,58 @@ const UserReportPage = () => {
         );
     };
     interface Report {
-        reporter: string;
-        reported: string;
-        content: string;
-        create_at: string;
+        _id: string;
+        reason: string;
+        status: string;
+        description: string;
+        createdAt: string;
+        reported_user: any;
+        reported_content: any
     }
     const initialData: Report = {
-        reporter: "",
-        reported: "",
-        content: "",
-        create_at: "",
+        _id: "",
+        reason: "",
+        status: "",
+        description: "",
+        createdAt: "",
+        reported_user: {},
+        reported_content: {}
     };
     const [dataDialog, setDataDialog] = useState(initialData);
     const dialog = (item: Report) => {
+        const handleRoleChange = () => {
+            let today: Date | null = new Date();
+            switch (judge) {
+                case "1":
+                    today.setDate(today.getDate() + 1);
+                    break;
+                case "2":
+                    today.setDate(today.getDate() + 3);
+                    break;
+                case "3":
+                    today.setDate(today.getDate() + 7);
+                    break;
+                case "4":
+                    today.setDate(today.getDate() + 30);
+                    break;
+                case "5":    
+                    today = null
+                    break;
+            }
+            console.log(today);
+            console.log(judge);
+            
+            const body = {
+                report_id: item._id,
+                status: judge === "0" ? "rejected" : "resolved",
+                date_of_judge: today
+            }
+            AxiosInstance().put('/report/report',body)
+                .then((res) => {
+                    setShowDialog(false);                    
+                    setData(data.filter((i) => i._id !== item._id));
+                })
+        };
         return (
             <>
                 <div className="fixed inset-0 bg-black bg-opacity-50 z-1"></div>
@@ -117,7 +169,7 @@ const UserReportPage = () => {
                                 Reporter:
                             </span>
                             <span className="text-base text-[#000] ml-10">
-                                {item.reporter}
+                                {item.reported_user.fullname}
                             </span>
                         </div>
                         <div>
@@ -125,7 +177,7 @@ const UserReportPage = () => {
                                 Reported user:
                             </span>
                             <span className="text-base text-[#000] ml-10">
-                                {item.reported}
+                                {item.reported_content.email}
                             </span>
                         </div>
                     </div>
@@ -134,7 +186,7 @@ const UserReportPage = () => {
                             Content:
                         </span>
                         <span className="text-base text-[#000] ml-10">
-                            {item.content}
+                            {item.reason}
                         </span>
                     </div>
                     <div className="mb-5">
@@ -154,26 +206,38 @@ const UserReportPage = () => {
                         </div>
                     </div>
                     <div className="flex justify-end gap-5">
-                        <button className="bg-yellow-400 hover:bg-yellow-500 text-white font-semibold py-2 px-4 rounded">
-                            Warning
-                        </button>
-
-                        <button className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded">
-                            Blocking
-                        </button>
-
-                        <button className="bg-black hover:bg-gray-800 text-white font-semibold py-2 px-4 rounded">
-                            Permanent Ban
-                        </button>
-
-                        <button
-                            className="border-[2px] border-[#6d6e6f] hover:bg-gray-200 text-black font-semibold py-2 px-4 rounded"
-                            onClick={() => {
-                                setShowDialog(false);
-                            }}
-                        >
-                            Cancel
-                        </button>
+                <div className='flex  items-center gap-10'>
+                    <div className='w-60'>
+                        <span className='text-lg font-semibold'>Date of judge</span>
+                    </div>
+                    <Select 
+                        // value={newAdmin.role} 
+                        onValueChange={(value) => setJudge(value)}
+                    >
+                        <SelectTrigger className="w-4/5">
+                            <SelectValue placeholder="Select role" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="0">No Infringement</SelectItem>
+                            <SelectItem value="1">1 day</SelectItem>
+                            <SelectItem value="2">3 days</SelectItem>
+                            <SelectItem value="3">7 days</SelectItem>
+                            <SelectItem value="4">1 month</SelectItem>
+                            <SelectItem value="5">Permanent</SelectItem>
+                        </SelectContent>
+                    </Select>
+                </div>
+                <button onClick={() => handleRoleChange()} className="bg-green-400 hover:bg-green-500 text-white font-semibold py-2 px-4 rounded">
+                    Handle
+                </button>
+                <button
+                    className="border-[2px] border-[#6d6e6f] hover:bg-gray-200 text-black font-semibold py-2 px-4 rounded"
+                    onClick={() => {
+                        setShowDialog(false);
+                    }}
+                >
+                    Cancel
+                </button>
                     </div>
                 </div>
             </>
@@ -195,29 +259,39 @@ const UserReportPage = () => {
                 </div>
             </div>
             <div className="flex flex-col gap-4">
-                {data.slice(indexS, indexE).map((item, index) => (
-                    <button
-                        key={index}
-                        className="flex flex-row w-full pt-2 pb-2 justify-between items-center pr-5 pl-5 border-[1px] border-[#C2D3FF] rounded-2xl"
-                        onClick={() => {
-                            setShowDialog(true);
-                            setDataDialog(item);
-                        }}
-                    >
-                        <div className="text-base font-regular text-[#797D8C] flex-[1] text-center">
-                            {item.create_at}
-                        </div>
-                        <div className="text-base font-regular text-[#797D8C] flex-[1] text-center truncate">
-                            {item.reporter}
-                        </div>
-                        <div className="text-sm font-semibold text-[#797D8C] flex-[1] text-center truncate">
-                            {item.reported}
-                        </div>
-                        <div className="text-sm font-regular text-[#797D8C] flex-[2]  truncate text-left">
-                            {item.content}
-                        </div>
-                    </button>
-                ))}
+                {(data && data.length > 0)&& data.slice(indexS, indexE).map((item, index) => {
+                    return (
+                        <button
+                            key={index}
+                            className = { 
+                                item.status === "pending" 
+                                    ? "flex flex-row w-full pt-2 pb-2 justify-between items-center pr-5 pl-5 border-[1px] border-[#C2D3FF] rounded-2xl" 
+                                    : (
+                                        item.status === "rejected" 
+                                            ? "flex flex-row w-full pt-2 pb-2 justify-between items-center pr-5 pl-5 border-[1px] border-[#FFC0C0] rounded-2xl" 
+                                            : "flex flex-row w-full pt-2 pb-2 justify-between items-center pr-5 pl-5 border-[1px] border-[#46f646] rounded-2xl"
+                                        )
+                            }
+                            onClick={() => {
+                                setShowDialog(true);
+                                setDataDialog(item);
+                            }}
+                >
+                    <div className="text-base font-regular text-[#797D8C] flex-[1] text-center">
+                        {item.createdAt}
+                    </div>
+                    <div className="text-base font-regular text-[#797D8C] flex-[1] text-center truncate">
+                        {item.reported_user.fullname}
+                    </div>
+                    <div className="text-sm font-semibold text-[#797D8C] flex-[1] text-center truncate">
+                        {item.reported_content.email}
+                    </div>
+                    <div className="text-sm font-regular text-[#797D8C] flex-[2]  truncate text-left">
+                        {item.reason}
+                    </div>
+                </button>
+                    )
+                })}
             </div>
             <div className="flex flex-row items-center justify-center my-5  ">
                 {pagination()}
---
+++ app/config/axiosInstance.js
@@ -0,0 +1,118 @@
+import axios from 'axios';
+// import {store} from '../store';
+// import {updateTokens} from '../store/slices';
+// import {logoutRef} from '../components/LogoutDialog';
+
+const BASEURL = 'http://192.168.1.10:8012/api';
+
+let isRefreshing = false;
+let queue = [];
+
+const AxiosInstance = (contentType = 'application/json') => {
+  const axiosInstance = axios.create({
+    baseURL: BASEURL,
+    timeout: 60000,
+  });
+
+  // const processQueue = (err, accessToken) => {
+  //   queue.forEach(request => {
+  //     if (err) {
+  //       request.reject(err);
+  //     } else {
+  //       request.resolve(accessToken);
+  //     }
+  //   });
+
+  //   queue = [];
+  // };
+
+  // const addRequest = originRequest => {
+  //   return new Promise((resolve, reject) => {
+  //     queue.push({resolve, reject});
+  //   })
+  //     .then(token => {
+  //       originRequest.headers.authorization = `Bearer ${token}`;
+  //       return axiosInstance(originRequest);
+  //     })
+  //     .catch(err => console.log('error: ', err));
+  // };
+
+  axiosInstance.interceptors.request.use(
+    async config => {
+      // let token;
+      // if (store.getState().account.token.accessToken) {
+      //   token = store.getState().account.token.accessToken;
+      // }
+
+      config.headers = {
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjcxZTZiNWVlZjg3YWQzYjkzYWRiMDkxIiwiaWF0IjoxNzMxMDM1NTIxLCJleHAiOjE3MzM2Mjc1MjF9.wgdsDzmvqebrIVhHDTjAhWdYU9o1OcpBpumVKz0zruY`,
+        Accept: 'application/json',
+        'Content-Type': contentType,
+        security: 'secure_code',
+      };
+      return config;
+    },
+    err => Promise.reject(err),
+  );
+  axiosInstance.interceptors.response.use(
+    res => res.data,
+    async err => {
+      // if (!err?.response || !err?.response?.data)
+      //   return await Promise.reject(err);
+
+      // const refreshToken = store.getState().account.token.refreshToken;
+      // const originRequest = err.config;
+
+      // if (
+      //   err.response?.status === 401 &&
+      //   err.response?.data?.message !== 'jwt expired'
+      // ) {
+      //   // logout
+      //   logoutRef.current?.showDialog();
+      // }
+
+      // if (
+      //   err.response.status === 401 &&
+      //   err.response.data.message === 'jwt expired'
+      // ) {
+      //   if (isRefreshing) {
+      //     return addRequest(originRequest);
+      //   }
+
+      //   isRefreshing = true;
+
+      //   try {
+      //     const res = await axiosInstance.post('/account/renew-tokens', {
+      //       refreshToken,
+      //     });
+
+      //     if (res) {
+      //       const payload = {
+      //         accessToken: res.data.token.accessToken,
+      //         refreshToken: res.data.token.refreshToken,
+      //       };
+
+      //       store.dispatch(updateTokens(payload));
+      //       processQueue(null, res.data.token.accessToken);
+      //       originRequest.headers.authorization = `Bearer ${res.data.token.accessToken}`;
+      //       return await axiosInstance(originRequest);
+      //     }
+      //   } catch (e) {
+      //     processQueue(e, null);
+      //     if (e.response.data.status === 401) {
+      //       logoutRef.current?.showDialog();
+      //       return;
+      //     }
+      //   } finally {
+      //     isRefreshing = false;
+      //   }
+      // }
+
+      return await Promise.reject(err);
+    },
+  );
+
+  return axiosInstance;
+};
+
+export default AxiosInstance;
